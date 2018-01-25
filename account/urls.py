f9510b10b8ca62a8ec3e6573a767996459ba2649
ZHOU <prince5460@126.com>
2018-01-25 15:15:39 +0800

    增加登录及注销功能

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='user_login')
+    url(r'^login/$', login, {'template_name': 'account/login.html'}, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout')
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
+
 def user_login(request):
     return None
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('blog:blog_title'))
