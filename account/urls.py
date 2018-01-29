2b8314a1645c9a8acee9fc2d9849a2e29187da72
ZHOU <prince5460@126.com>
2018-01-29 20:30:20 +0800

    添加修改密码功能

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     # url(r'^logout/$', views.user_logout, name='user_logout'),
     url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
     url(r'^register/$', views.register, name='user_register'),
+    url(r'^password_change/$', auth_views.password_change,
+        # {"template_name": "account/password_change.html"},
+        {'post_change_redirect': '/account/password_change_done'},
+        name='password_change'),
+    url(r'^password_change_done/$', auth_views.password_change_done,
+        # {'template_name': 'account/password_change_done.html'},
+        name='password_change_done'),
 ]
